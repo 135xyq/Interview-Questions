5536d23fc927a7cbaaae5db1aefb4b19c73a1ab7
xyq <xyq135@gmail.com>
2022-08-18 10:08:57 +0800

    node-测试代码-全局对象

---
+++ node/测试代码/全局对象.js
@@ -0,0 +1,35 @@
+// setTimeout(()=>{
+//     console.log("setTimeout")
+// },1000)
+
+// const timer = setInterval(()=>{
+//     console.log("setInterval")
+// },1000)
+
+
+// console.log("获取当前目录",__dirname);
+
+// console.log("文件路径：",__filename)
+
+
+// const buffer = Buffer.from("谢永强")
+
+// console.log(buffer)
+
+// console.log(process.cwd())
+
+
+// setInterval(()=>{
+//     console.log("测试exit")
+// },1000)
+
+// setTimeout(()=>{
+//     process.exit();//五秒后结束运行程序
+// },5000)
+
+
+// console.log(process.argv)
+
+// console.log("当前操作系统：",process.platform)
+
+console.log("获取环境变量：",process.env)
