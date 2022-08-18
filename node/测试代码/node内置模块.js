2704496c63f4ded01f21baf90a034c7b923f45bf
xyq <xyq135@gmail.com>
2022-08-18 10:38:04 +0800

    node-测试代码-内置模块

---
+++ node/测试代码/node内置模块.js
@@ -0,0 +1,21 @@
+const os = require("os");
+
+const path = require("path")
+
+// console.log(123,os.EOL,123)
+
+// console.log("CPU架构名：",os.arch())
+
+// console.log("cpu内核：",os.cpus())
+
+// console.log("当前空闲内存：",os.freemem())
+
+// console.log("用户目录：",os.homedir())
+
+// console.log("用户主机名：",os.hostname())
+
+// console.log(path.sep)
+
+console.log(path.join("a","b","c"))
+
+console.log(path.resolve("./node内置模块.js"))
