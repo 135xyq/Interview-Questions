6970ed0dc9110093f7e0b6f8a60ec133af6460a9
xyq <xyq135@gmail.com>
2022-08-27 15:24:09 +0800

    js-测试代码-JavaScript基础知识-位运算

---
+++ js/测试代码/JavaScript基础/位运算.js
@@ -0,0 +1,13 @@
+const a = 11;//1011
+const b = 7;//111
+
+console.log("|",a|b)
+console.log("&",a&b)
+
+console.log("^",a^b)
+
+console.log("~",~b)
+
+console.log("<<",a<<2)
+
+console.log(">>",b>>2)
