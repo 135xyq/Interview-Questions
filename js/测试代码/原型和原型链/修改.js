c4c83601067ecc126147008cb7f9ed71c981f930
xyq <xyq135@gmail.com>
2022-08-28 21:57:04 +0800

    js-测试代码-原型和原型链-修改原型

---
+++ js/测试代码/原型和原型链/修改.js
@@ -0,0 +1,19 @@
+function Person(name) {
+	this.name = name;
+}
+// 修改原型
+Person.prototype.getName = function () {};
+var p = new Person("hello");
+console.log(p.__proto__ === Person.prototype); // true
+console.log(p.__proto__ === p.constructor.prototype); // true
+
+// 重写原型
+Person.prototype = {
+	getNames: function () {},
+};
+var p = new Person("hello");
+console.log(p.__proto__ === Person.prototype); // true
+console.log(p.__proto__)
+console.log(p.constructor.prototype)
+console.log(p.constructor)
+console.log(p.__proto__ === p.constructor.prototype); // false
