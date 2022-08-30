e7383cfaf312d73d89d73ca4f75d80ace4f2fbec
xyq <xyq135@gmail.com>
2022-08-30 13:47:06 +0800

    promise-测试代码-代码输出题-2

---
+++ promise/测试代码/代码输出题/4.js
@@ -0,0 +1,11 @@
+async function m() {
+	const n = await 1;
+	console.log(n);
+}
+
+m();
+console.log(2);
+
+
+// 2
+// 1
---
+++ promise/测试代码/代码输出题/5.js
@@ -0,0 +1,15 @@
+async function m() {
+	const n = await 1;
+	console.log(n);
+}
+
+(async () => {
+	await m();
+	console.log(2);
+})();
+
+console.log(3);
+
+// 3
+// 1
+// 2
