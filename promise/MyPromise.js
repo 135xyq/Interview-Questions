2d2e2077814324876e73a6bd8dcc4b84053726f0
xyq <xyq135@gmail.com>
2022-08-30 18:00:16 +0800

    promise-测试代码-手写promise-将一个函数放入微队列执行

--- promise/MyPromise.js
+++ promise/MyPromise.js
@@ -2,6 +2,28 @@
 const PENDING = "pending";
 const FULFILLED = "fulfilled";
 const REJECTED = "rejected";
+
+/**
+ * 将函数加入微队列执行
+ * @param {function} callback
+ */
+function runMicroTask(callback) {
+	if (process && process.nextTick) {
+		// node环境下执行
+		process.nextTick(callback);
+	} else if (MutationObserver) {
+		// 在浏览器环境下执行
+		const p = document.createElement("p");
+		const observer = new MutationObserver(callback);
+		observer.observe(p, {
+			childList: true, // 观察该元素内部的变化
+		});
+		p.innerHTML = "1";
+	} else {
+		setTimeout(callback, 0);
+	}
+}
+
 /**
  *
  *
@@ -16,12 +38,12 @@ class MyPromise {
 		this._state = PENDING; //初始状态
 		this._value = undefined; //初始数据
 
-        // 处理错误，报错直接推向reject
+		// 处理错误，报错直接推向reject
 		try {
 			executor(this._resolve.bind(this), this._reject.bind(this));
 		} catch (error) {
-            this._reject(error)
-        }
+			this._reject(error);
+		}
 	}
 
 	/**
@@ -30,10 +52,10 @@ class MyPromise {
 	 * @param {*} data 数据
 	 */
 	_changeState(state, data) {
-        // 状态只能改变一次
-        if(this._state !== PENDING){
-            return;
-        }
+		// 状态只能改变一次
+		if (this._state !== PENDING) {
+			return;
+		}
 		this._state = state;
 		this._value = data;
 	}
@@ -53,10 +75,18 @@ class MyPromise {
 	_reject(reason) {
 		this._changeState(REJECTED, reason);
 	}
+
+	/**
+	 * then方法
+	 * @param {function} handleResolve resolve状态的后续处理函数
+	 * @param {function} handleReject reject状态的后续处理函数
+	 * @returns 返回一个新的promise
+	 */
+	then(handleResolve, handleReject) {
+		return new MyPromise((resolve, reject) => {});
+	}
 }
 
 const promise = new MyPromise((resolve, reject) => {
-	throw new Error(123)
+	throw new Error(123);
 });
-
-console.log(promise);
