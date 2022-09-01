9c57321ff7f2e019b7447407931f6f932deaab3b
xyq <xyq135@gmail.com>
2022-09-01 19:18:35 +0800

    代码输出题-原型&继承 1-3

---
+++ 代码输出题/原型&继承/3.js
@@ -0,0 +1,17 @@
+var F = function() {};
+Object.prototype.a = function() {
+  console.log('a');
+};
+Function.prototype.b = function() {
+  console.log('b');
+}
+var f = new F();
+f.a();
+f.b();
+F.a();
+F.b()
+
+// a
+// Uncaught TypeError: f.b is not a function
+// a
+// b
