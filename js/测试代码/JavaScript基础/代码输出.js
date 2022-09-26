7df50bd4661fa3d3a0b166f1c5ba6ff57ee31b69
xyq <xyq135@gmail.com>
2022-09-26 15:29:19 +0800

    js-测试代码-JavaScript-代码输出题

--- js/测试代码/JavaScript基础/代码输出.js
+++ js/测试代码/JavaScript基础/代码输出.js
@@ -1,24 +1,44 @@
-// function a() {
-//     console.log(1);
-// }
-// (function() {
-//     if (false) {
-//         function a() {
-//             console.log(2);
-//         }
-//     }
-//     console.log(typeof a); 
-//     a(); 
-// })()
+/* function a() {
+    console.log(1);
+}
+(function() {
+    if (false) {
+        function a() {
+            console.log(2);
+        }
+    }
+    console.log(typeof a); 
+    a(); 
+})()
 
 
+ */
 
-const arr = [1,2,,4,5];
+/* const arr = [1, 2, , 4, 5];
 
-let b = arr.map(item=>item+1);
+let b = arr.map((item) => item + 1);
 
-arr.forEach(item=>{
-    console.log(item)
-})
+arr.forEach((item) => {
+	console.log(item);
+});
 
-console.log(b,arr)
+console.log(b, arr);
+ */
+
+/* 
+let arr =  [1,2,3]
+let arr2 = [4,5]
+
+let arr1 = arr.concat(arr2)
+
+console.log(arr1,arr,arr2) */
+
+/* const obj = { a: "one", b: "two", a: "three" };
+console.log(obj);
+ */
+
+var b = 10;
+(function b() {
+  b = 20;
+  console.log(b)
+})()
