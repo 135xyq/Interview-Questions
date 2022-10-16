0a9bc6c4cd874d18f4c21446c467baa8661d1e34
xyq <xyq135@gmail.com>
2022-10-16 09:00:36 +0800

    vue-代码测试-learn 事件总线

--- vue/代码测试/learn/src/components/事件总线/Test.vue
+++ vue/代码测试/learn/src/components/事件总线/Test.vue
@@ -2,15 +2,18 @@
 	<div>
 		<FirstCom></FirstCom>
 		<SecondCom></SecondCom>
+		<third-com></third-com>
 	</div>
 </template>
 <script>
 import FirstCom from "@/components/事件总线/FirstCom";
 import SecondCom from "@/components/事件总线/secondCom";
+import thirdCom from "@/components/事件总线/thirdCom";
 export default {
   components: {
 	  FirstCom,
-	  SecondCom
+	  SecondCom,
+	  thirdCom
   },
   data() {
     return {}
---
+++ vue/代码测试/learn/src/components/事件总线/thirdCom.vue
@@ -0,0 +1,37 @@
+<template>
+	<div>
+		<p>num: {{data}}</p>
+		<p>
+			<button @click="add">+</button>
+		</p>
+	</div>
+</template>
+
+<script>
+import {EventBus} from "@/components/事件总线/eventBus";
+
+export default {
+	name: "thirdCom",
+	data() {
+		return {
+			data: 0
+		}
+	},
+	mounted(){
+		EventBus.$on("addition",par=>{
+			this.data = par.num;
+		})
+	},
+	methods:{
+		add(){
+			EventBus.$emit("addition",{
+				num:this.data+1
+			})
+		}
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
